79215f7d9f8b037f7f54591f04e79499b44adf44
whatusernameisnttaken <matteo.kastler@gmail.com>
2020-04-27 17:30:48 +0200

    update db class

--- src/Db.java
+++ src/Db.java
@@ -1,7 +1,19 @@
 package src;
 
-public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import src.utils.PropertyReader;
+import java.io.IOException;
+
+public class Db {
+	
+	public Db() throws IOException, ClassNotFoundException, SQLException{
+		PropertyReader rd = new PropertyReader("connection.properties");
+		String user = rd.get("user");
+		String pwd = rd.get("password");
+		String URL = rd.get("URL");
         Class.forName("com.mysql.jdbc.Driver");
-    }
+        Connection con = DriverManager.getConnection(URL,user,pwd);
+	}
 }
