b0f509264fc0bd3e5b5d79ea1a31d45f67e8b965
whatusernameisnttaken <matteo.kastler@gmail.com>
2020-04-27 18:07:08 +0200

    added main

--- src/Db.java
+++ src/Db.java
@@ -2,11 +2,15 @@ package src;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 import src.utils.PropertyReader;
 import java.io.IOException;
 
 public class Db {
+	Connection con;
 	
 	public Db() throws IOException, ClassNotFoundException, SQLException{
 		PropertyReader rd = new PropertyReader("connection.properties");
@@ -14,6 +18,15 @@ public class Db {
 		String pwd = rd.get("password");
 		String URL = rd.get("URL");
         Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection(URL,user,pwd);
+        con = DriverManager.getConnection(URL,user,pwd);
+	}
+	
+	public void test() throws SQLException{
+		Statement stmt = con.createStatement();
+		ResultSet result = stmt.executeQuery("show tables");
+		while(result.next()){
+			System.out.println(result.getString("Tables_in_m3u4u"));
+		}
+		
 	}
 }
