16306f6373a67cf60a6cc6782d2df4b82b785ec7
perktold <felix.perktold@live.at>
2020-04-27 18:52:31 +0200

    added File Class

---
+++ src/Types/File.java
@@ -0,0 +1,11 @@
+package src.Types;
+
+public class File {
+    private int songId;
+    private String filePath;
+
+    public File(int songId, String filePath){
+        this.songId = songId;
+        this.filePath = filePath;
+    }
+}
