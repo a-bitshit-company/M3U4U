458f696623586dec0c0032542da5ea24471ec41f
whatusernameisnttaken <matteo.kastler@gmail.com>
2020-04-27 18:31:25 +0200

    add types

--- src/Types/Playlist.java
+++ src/Types/Playlist.java
@@ -4,6 +4,14 @@ public class Playlist {
 	private String name;
 	private String genre;
 	private String desc;
-	private int PlaylistId;
+	private int playlistId;
+	
+	public Playlist(String name, String genre, String desc, int playlistId){
+		this.name = name;
+		this.genre = genre;
+		this.desc = desc;
+		this.playlistId = playlistId;
+		
+	}
 	
 }
